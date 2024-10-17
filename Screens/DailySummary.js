da1121c7de9bc9e4fe705b2723abd73143854793
NK <kurbetnk@gmail.com>
2024-10-17 16:32:11 -0400

    Database linked to summary screens

--- Screens/DailySummary.js
+++ Screens/DailySummary.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
 import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 
 export default function DailySummary() {
+
+  const [legalHours, setLegalHours] = useState(0);
+  const [cashHours, setCashHours] = useState(0);
+  const [legalPay, setLegalPay] = useState(0);
+  const [cashPay, setCashPay] = useState(0);
+
+  useEffect(() => {
+    getDailyDataFromDB();
+  }, [])
+
+  const getDailyDataFromDB = async () => {
+    //get the legal hours from the current day
+    const getDayRef = collection(db, 'daily', auth.currentUser.email, getCurrentDate());
+    const getDayDoc = query(getDayRef, where('dayNum', '==', getCurrentDate()));
+    const getDayData = await getDocs(getDayDoc);
+    if (getDayData.docs.length === 0) {
+      setLegalHours(0);
+      setCashHours(0);
+      setLegalPay(0);
+      setCashPay(0);
+    } else {
+      setLegalHours(getDayData.docs[0].data().legalHours);
+      setCashHours(getDayData.docs[0].data().cashHours);
+      setLegalPay(getCashPayData.docs[0].data().legalPay);
+      setCashPay(getCashPayData.docs[0].data().cashPay);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
@@ -14,15 +42,15 @@ export default function DailySummary() {
             </View>
             <View style={styles.calculationRow}>
               <Text style={styles.calculationLabel}>Legal Hours:</Text>
-              <Text style={styles.calculationValue}>20</Text>
+              <Text style={styles.calculationValue}>{legalHours}</Text>
             </View>
             <View style={styles.calculationRow}>
               <Text style={styles.calculationLabel}>Cash Hours:</Text>
-              <Text style={styles.calculationValue}>10</Text>
+              <Text style={styles.calculationValue}>{cashHours}</Text>
             </View>
             <View style={[styles.calculationRow, styles.totalRow]}>
               <Text style={styles.calculationLabel}>Total Hours:</Text>
-              <Text style={styles.calculationValue}>30</Text>
+              <Text style={styles.calculationValue}>{legalHours + cashHours}</Text>
             </View>
           </View>
           <View style={styles.calculationCard}>
@@ -32,15 +60,15 @@ export default function DailySummary() {
             </View>
             <View style={styles.calculationRow}>
               <Text style={styles.calculationLabel}>Legal Pay:</Text>
-              <Text style={styles.calculationValue}>$ 80</Text>
+              <Text style={styles.calculationValue}>$ {legalPay}</Text>
             </View>
             <View style={styles.calculationRow}>
               <Text style={styles.calculationLabel}>Cash Pay:</Text>
-              <Text style={styles.calculationValue}>$ 40</Text>
+              <Text style={styles.calculationValue}>$ {cashPay}</Text>
             </View>
             <View style={[styles.calculationRow, styles.totalRow]}>
               <Text style={styles.calculationLabel}>Total Pay:</Text>
-              <Text style={styles.calculationValue}>$ 120</Text>
+              <Text style={styles.calculationValue}>$ {legalPay + cashPay}</Text>
             </View>
           </View>
         </View>
